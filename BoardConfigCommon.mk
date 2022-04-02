7861ee195d745133d3abc0cedd4e9363a61d0bcc
Nathanael Robbins <nejrobbins@gmail.com>
2022-04-01 21:35:16 -0400

    sm8150-common: use gulch_defconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,7 +35,7 @@ BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-KERNEL_DEFCONFIG := vendor/sm8150-perf_defconfig
+KERNEL_DEFCONFIG := gulch_defconfig
 
 # A/B
 AB_OTA_UPDATER := true
